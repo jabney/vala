dddd8960283f63f2c7f6cb4a15ab1e798feda9fd
James Abney <james.abney@gmail.com>
2017-12-29 10:06:30 -0800

    Add html and png rules

--- demo/webpack.config.js
+++ demo/webpack.config.js
@@ -1,6 +1,7 @@
 
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 
 module.exports = {
   entry: './demo/src/app.js',
@@ -9,9 +10,16 @@ module.exports = {
     path: path.resolve(__dirname, './dist')
   },
   module: {
-    rules: []
+    rules: [{
+      test: /\.png$/,
+      loader: 'file-loader?name=[name].[hash:8].[ext]'
+    }, {
+      test: /\.html$/,
+      loader: 'html-loader'
+    }]
   },
   plugins: [
+    new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
       template: 'demo/src/index.html'
     })
